1dad5ef27f6cc7d5c136c3513338faa97c752563
Brandon Shroyer <bdshroyer@gmail.com>
2021-12-15 22:04:30 -0500

    Linting changes to matcher.

--- matchers/node_matcher.go
+++ matchers/node_matcher.go
@@ -1,47 +1,44 @@
 package matchers
 
 import (
-    "fmt"
-    "reflect"
+	"fmt"
+	"reflect"
 
-    "github.com/onsi/gomega/types"
-    "gonum.org/v1/gonum/graph"
+	"github.com/onsi/gomega/types"
+	"gonum.org/v1/gonum/graph"
 )
 
 type NodeMatcher struct {
-    expected interface{}
+	expected interface{}
 }
 
 func MatchNode(expected interface{}) types.GomegaMatcher {
-    return &NodeMatcher{expected: expected}
+	return &NodeMatcher{expected: expected}
 }
 
 func (nm *NodeMatcher) Match(actual interface{}) (success bool, err error) {
-        actualNode, ok := actual.(graph.Node)
-        if !ok {
-            return false, fmt.Errorf("NodeMatcher requires an actual input that implements the Node interface.")
-        }
+	actualNode, ok := actual.(graph.Node)
+	if !ok {
+		return false, fmt.Errorf("NodeMatcher requires an actual input that implements the Node interface")
+	}
 
-        expectedNode, ok := nm.expected.(graph.Node)
-        if !ok {
-            return false, fmt.Errorf("NodeMatcher requires an expected input that implements the Node interface.")
-        }
+	expectedNode, ok := nm.expected.(graph.Node)
+	if !ok {
+		return false, fmt.Errorf("NodeMatcher requires an expected input that implements the Node interface")
+	}
 
+	assignable := reflect.TypeOf(actual).AssignableTo(reflect.TypeOf(nm.expected))
+	if !assignable {
+		return false, fmt.Errorf("%T cannot be assigned to %T", actual, nm.expected)
+	}
 
-        assignable := reflect.TypeOf(actual).AssignableTo(reflect.TypeOf(nm.expected))
-        if !assignable {
-            return false, fmt.Errorf("%T cannot be assigned to %T.", actual, nm.expected)
-        }
-
-        return expectedNode.ID() == actualNode.ID(), nil
+	return expectedNode.ID() == actualNode.ID(), nil
 }
 
 func (nm *NodeMatcher) FailureMessage(actual interface{}) (message string) {
-    return fmt.Sprintf("Expected \n\t%#v\nto have an ID matching that of \n\t%#v", nm.expected, actual)
+	return fmt.Sprintf("Expected \n\t%#v\nto have an ID matching that of \n\t%#v", nm.expected, actual)
 }
 
 func (nm *NodeMatcher) NegatedFailureMessage(actual interface{}) (message string) {
-    return fmt.Sprintf("Expected \n\t%#v\nnot to have an ID matching that of \n\t%#v", nm.expected, actual)
+	return fmt.Sprintf("Expected \n\t%#v\nnot to have an ID matching that of \n\t%#v", nm.expected, actual)
 }
-
-
--- matchers/node_matcher_test.go
+++ matchers/node_matcher_test.go
@@ -1,12 +1,11 @@
 package matchers_test
 
 import (
-    . "github.com/onsi/gomega"
-    . "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
-    "github.com/bdshroyer/burrow/matchers"
+	"github.com/bdshroyer/burrow/matchers"
 )
-
 
 type NodeType1 struct {
 	Val int64
@@ -24,55 +23,54 @@ func (n *NodeType2) ID() int64 {
 	return n.Val
 }
 
-
 var _ = Describe("NodeMatcher", func() {
-    It("Matches nodes with identical values.", func() {
-        expected := &NodeType1{Val: int64(2)}
-        actual := &NodeType1{Val: int64(2)}
-        Expect(actual).To(matchers.MatchNode(expected))
-    })
+	It("Matches nodes with identical values.", func() {
+		expected := &NodeType1{Val: int64(2)}
+		actual := &NodeType1{Val: int64(2)}
+		Expect(actual).To(matchers.MatchNode(expected))
+	})
 
-    It("Does not match nodes with different values.", func() {
-        expected := &NodeType1{Val: int64(2)}
-        actual := &NodeType1{Val: int64(3)}
-        Expect(actual).NotTo(matchers.MatchNode(expected))
-    })
+	It("Does not match nodes with different values.", func() {
+		expected := &NodeType1{Val: int64(2)}
+		actual := &NodeType1{Val: int64(3)}
+		Expect(actual).NotTo(matchers.MatchNode(expected))
+	})
 
-    Context("When the expected input is not a node", func() {
-        It("should raise an error", func() {
-            expected := "oink"
-            actual := &NodeType1{Val: int64(3)}
+	Context("When the expected input is not a node", func() {
+		It("should raise an error", func() {
+			expected := "oink"
+			actual := &NodeType1{Val: int64(3)}
 
-            match, err := matchers.MatchNode(expected).Match(actual)
+			match, err := matchers.MatchNode(expected).Match(actual)
 
-            Expect(match).To(BeFalse())
-            Expect(err).To(MatchError("NodeMatcher requires an expected input that implements the Node interface."))
-        })
-    })
+			Expect(match).To(BeFalse())
+			Expect(err).To(MatchError("NodeMatcher requires an expected input that implements the Node interface"))
+		})
+	})
 
-    Context("When the actual input is not a node", func() {
-        It("should raise an error", func() {
-            expected := &NodeType1{Val: int64(2)}
-            actual:= "quack"
+	Context("When the actual input is not a node", func() {
+		It("should raise an error", func() {
+			expected := &NodeType1{Val: int64(2)}
+			actual := "quack"
 
-            match, err := matchers.MatchNode(expected).Match(actual)
+			match, err := matchers.MatchNode(expected).Match(actual)
 
-            Expect(match).To(BeFalse())
-            Expect(err).To(MatchError("NodeMatcher requires an actual input that implements the Node interface."))
-        })
-    })
+			Expect(match).To(BeFalse())
+			Expect(err).To(MatchError("NodeMatcher requires an actual input that implements the Node interface"))
+		})
+	})
 
-    // Fails when the actual is unassignable to the expected, but NOT the other way around.
-    // This allows us to expect against a DeliveryNode interface.
-    Context("When the actual type doesn't match the expected type", func() {
-        It("should raise an error if the actual type can't be assigned to the expected type.", func() {
-            expected := &NodeType1{Val: int64(2)}
-            actual:= &NodeType2{Val: int64(2)}
+	// Fails when the actual is unassignable to the expected, but NOT the other way around.
+	// This allows us to expect against a DeliveryNode interface.
+	Context("When the actual type doesn't match the expected type", func() {
+		It("should raise an error if the actual type can't be assigned to the expected type.", func() {
+			expected := &NodeType1{Val: int64(2)}
+			actual := &NodeType2{Val: int64(2)}
 
-            match, err := matchers.MatchNode(expected).Match(actual)
+			match, err := matchers.MatchNode(expected).Match(actual)
 
-            Expect(match).To(BeFalse())
-            Expect(err).To(MatchError("*matchers_test.NodeType2 cannot be assigned to *matchers_test.NodeType1."))
-        })
-    })
+			Expect(match).To(BeFalse())
+			Expect(err).To(MatchError("*matchers_test.NodeType2 cannot be assigned to *matchers_test.NodeType1"))
+		})
+	})
 })
