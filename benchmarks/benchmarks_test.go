e4782f2a322723e576ae2faa126656c56d903384
Brandon Shroyer <bdshroyer@gmail.com>
2022-04-11 15:20:26 -0400

    Remove other-branch cruft from benchmarks.

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -37,10 +37,36 @@ var _ = Describe("Benchmark", func() {
 					Expect(err).NotTo(HaveOccurred())
 
 					stopwatch := experiment.NewStopwatch()
-					_, err = burrow.MakeDeliveryNetwork(1, 10000, distro)
+					G, err := burrow.MakeDeliveryNetwork(1, 10000, distro)
 					stopwatch.Record("Creation Time", gmeasure.Precision(time.Microsecond))
 
 					Expect(err).NotTo(HaveOccurred())
+
+					stopwatch.Reset()
+					nodes := G.Nodes()
+					stopwatch.Record("Node Gathering Time", gmeasure.Precision(time.Microsecond))
+
+					stopwatch.Reset()
+					for nodes.Next() {
+					}
+					stopwatch.Record("Node Iterating Time", gmeasure.Precision(time.Microsecond))
+
+					stopwatch.Reset()
+					edges := G.Edges()
+					stopwatch.Record("Edge Gathering Time", gmeasure.Precision(time.Microsecond))
+
+					nEdges := float64(edges.Len())
+					nStops := float64(len(G.Stops))
+					nHubs := float64(len(G.Hubs))
+
+					stopwatch.Reset()
+					for edges.Next() {
+						edges.WeightedEdge()
+					}
+					stopwatch.Record("Edge Iterating Time", gmeasure.Precision(time.Microsecond))
+
+					experiment.RecordValue("nEdges", nEdges, gmeasure.Units("edges"))
+					experiment.RecordValue("edge density", nEdges/(2*nHubs*nStops+(nStops*(nStops-1))/2))
 				},
 				gmeasure.SamplingConfig{N: 10},
 			)
