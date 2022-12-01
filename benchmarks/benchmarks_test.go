451cf8f63b5e639f0f404fa78d42f1de960427d8
Brandon Shroyer <bdshroyer@gmail.com>
2022-12-01 01:14:46 -0500

    Add bounded Gaussian distribution test to benchmarks.

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -26,57 +26,86 @@ func Today() time.Time {
 }
 
 var _ = Describe("Benchmark", func() {
-	Context("Create a 10000-stop delivery network with one hub", func() {
-		It("Runs on one core", func() {
-			experiment := gmeasure.NewExperiment("Network Creation")
+	benchmarkNetworkCreation := func(experiment *gmeasure.Experiment, cfg burrow.DeliveryNetworkConfig) func(int) {
+		return func(idx int) {
+			stopwatch := experiment.NewStopwatch()
+			G, err := burrow.MakeDeliveryNetwork(cfg)
+			stopwatch.Record("Creation Time", gmeasure.Precision(time.Microsecond))
+
+			Expect(err).NotTo(HaveOccurred())
+
+			stopwatch.Reset()
+			nodes := G.Nodes()
+			stopwatch.Record("Node Gathering Time", gmeasure.Precision(time.Microsecond))
+
+			stopwatch.Reset()
+			for nodes.Next() {
+			}
+			stopwatch.Record("Node Iterating Time", gmeasure.Precision(time.Microsecond))
+
+			stopwatch.Reset()
+			edges := G.Edges()
+			stopwatch.Record("Edge Gathering Time", gmeasure.Precision(time.Microsecond))
+
+			nEdges := float64(edges.Len())
+			nStops := float64(len(G.Stops))
+			nHubs := float64(len(G.Hubs))
+
+			stopwatch.Reset()
+			for edges.Next() {
+			}
+			stopwatch.Record("Edge Iterating Time", gmeasure.Precision(time.Microsecond))
+
+			meanOutDegree := float64(0.0)
+			for _, edges := range G.DEdges {
+				meanOutDegree += float64(len(edges)) / nStops
+			}
+
+			experiment.RecordValue("nEdges", nEdges, gmeasure.Units("edges"))
+			experiment.RecordValue("delivery network edge density", nEdges/(2*nHubs*nStops+(nStops*(nStops-1))/2))
+			experiment.RecordValue("mean out-degree", meanOutDegree)
+		}
+	}
+
+	Context("Create a delivery network with five hubs and 10000 stops", func() {
+		It("Runs on a uniform distribution", func() {
+			experiment := gmeasure.NewExperiment("Network Creation [uniform]")
 			AddReportEntry(experiment.Name, experiment)
 
+			distro, err := burrow.UniformTimestampDistribution(Today(), 24*time.Hour)
+			Expect(err).NotTo(HaveOccurred())
+
+			cfg := burrow.DeliveryNetworkConfig{HubNodes: 5, StopNodes: 10000, Distro: distro}
+
 			experiment.Sample(
-				func(idx int) {
-					distro, err := burrow.UniformTimestampDistribution(Today(), 24*time.Hour)
-					Expect(err).NotTo(HaveOccurred())
-
-					cfg := burrow.DeliveryNetworkConfig{HubNodes: 5, StopNodes: 10000, Distro: distro}
-
-					stopwatch := experiment.NewStopwatch()
-					G, err := burrow.MakeDeliveryNetwork(cfg)
-					stopwatch.Record("Creation Time", gmeasure.Precision(time.Microsecond))
-
-					Expect(err).NotTo(HaveOccurred())
-
-					stopwatch.Reset()
-					nodes := G.Nodes()
-					stopwatch.Record("Node Gathering Time", gmeasure.Precision(time.Microsecond))
-
-					stopwatch.Reset()
-					for nodes.Next() {
-					}
-					stopwatch.Record("Node Iterating Time", gmeasure.Precision(time.Microsecond))
-
-					stopwatch.Reset()
-					edges := G.Edges()
-					stopwatch.Record("Edge Gathering Time", gmeasure.Precision(time.Microsecond))
-
-					nEdges := float64(edges.Len())
-					nStops := float64(len(G.Stops))
-					nHubs := float64(len(G.Hubs))
-
-					stopwatch.Reset()
-					for edges.Next() {
-					}
-					stopwatch.Record("Edge Iterating Time", gmeasure.Precision(time.Microsecond))
-
-					meanOutDegree := float64(0.0)
-					for _, edges := range G.DEdges {
-						meanOutDegree += float64(len(edges)) / nStops
-					}
-
-					experiment.RecordValue("nEdges", nEdges, gmeasure.Units("edges"))
-					experiment.RecordValue("edge density", nEdges/(2*nHubs*nStops+(nStops*(nStops-1))/2))
-					experiment.RecordValue("mean out degree", meanOutDegree)
-				},
+				benchmarkNetworkCreation(experiment, cfg),
 				gmeasure.SamplingConfig{N: 10},
 			)
 		})
+
+		It("Runs on a Gaussian distribution with bounds", func() {
+			experiment := gmeasure.NewExperiment("Network Creation [Gaussian]")
+			AddReportEntry(experiment.Name, experiment)
+
+			distro, err := burrow.GaussianTimestampDistribution(
+				Today().Add(11*time.Hour),
+				2*time.Hour,
+			)
+
+			Expect(err).NotTo(HaveOccurred())
+
+			cfg := burrow.DeliveryNetworkConfig{
+				HubNodes:   5,
+				StopNodes:  10000,
+				Distro:     distro,
+				EdgeBounds: &burrow.TimeBox{0 * time.Hour, 6 * time.Hour},
+			}
+
+			experiment.Sample(
+				benchmarkNetworkCreation(experiment, cfg),
+				gmeasure.SamplingConfig{N: 10},
+			)
+		})
+
 	})
 })
